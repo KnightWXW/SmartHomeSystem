613fcf5d6f585111386b777e4df96f0b6b129350
KnightWXW <knight960903@gmail.com>
2025-11-08 12:17:29 +0800

    SmartHome

--- Hardware/DHT11.c
+++ Hardware/DHT11.c
@@ -1,21 +1,27 @@
-#include "dht11.h"
+#include "DHT11.h"
 #include "delay.h"
 
 #define DHT_PIN GPIO_Pin_1
 
-// 复位DHT11
-void DHT11_Rst(void)
+/**
+ * @description: 复位DHT11
+ * @return: {*}
+ */
+void DHT11_Reset(void)
 {
     DHT11_IO_OUT();  // SET OUTPUT
     DHT11_DQ_OUT(0); // 拉低DQ
-    DelayXms(20);    // 拉低至少18ms
+    Delay_ms(20);    // 拉低至少18ms
     DHT11_DQ_OUT(1); // DQ=1
-    DelayUs(30);     // 主机拉高20~40us
+    Delay_us(30);     // 主机拉高20~40us
 }
 
-// 等待DHT11的回应
-// 返回1:未检测到DHT11的存在
-// 返回0:存在
+/**
+ * @description: 等待DHT11的回应
+ * @return: {*}
+ *      1: 未检测到DHT11存在
+ *      0: 检测到DHT11存在
+ */
 uint8_t DHT11_Check(void)
 {
     uint8_t retry = 0;
@@ -23,68 +29,83 @@ uint8_t DHT11_Check(void)
     while (DHT11_DQ_IN && retry < 100) // DHT11会拉低40~80us
     {
         retry++;
-        DelayUs(1);
+        Delay_us(1);
     };
     if (retry >= 100)
+    {
         return 1;
+    }
     else
+    {
         retry = 0;
+    }
     while (!DHT11_DQ_IN && retry < 100) // DHT11拉低后会再次拉高40~80us
     {
         retry++;
-        DelayUs(1);
+        Delay_us(1);
     };
     if (retry >= 100)
+    {
         return 1;
+    }
     return 0;
 }
 
-// 从DHT11读取一个位
-// 返回值：1/0
+/**
+ * @description: 从DHT11读取一个数据位
+ * @return: {*}
+ */
 uint8_t DHT11_Read_Bit(void)
 {
     uint8_t retry = 0;
     while (DHT11_DQ_IN && retry < 100) // 等待变为低电平
     {
         retry++;
-        DelayUs(1);
+        Delay_us(1);
     }
     retry = 0;
     while (!DHT11_DQ_IN && retry < 100) // 等待变高电平
     {
         retry++;
-        DelayUs(1);
+        Delay_us(1);
     }
-    DelayUs(40); // 等待40us
+    Delay_us(40); // 等待40us
     if (DHT11_DQ_IN)
+    {
         return 1;
+    }
     else
+    {
         return 0;
+    }
 }
 
-// 从DHT11读取一个字节
-// 返回值：读到的数据
+/**
+ * @description: 从DHT11读取一个字节
+ * @return: {*}
+ */
 uint8_t DHT11_Read_Byte(void)
 {
-    uint8_t i, dat;
-    dat = 0;
-    for (i = 0; i < 8; i++)
+    uint8_t readByte = 0;
+    for (uint8_t i = 0; i < 8; i++)
     {
-        dat <<= 1;
-        dat |= DHT11_Read_Bit();
+        readByte <<= 1;
+        readByte |= DHT11_Read_Bit();
     }
-    return dat;
+    return readByte;
 }
 
-// 从DHT11读取一次数据
-// temp:温度值(范围:0~50°)
-// humi:湿度值(范围:20%~90%)
-// 返回值：0,正常;1,读取失败
+/**
+ * @description: 从DHT11读取一次数据
+ * @param {uint8_t} *temp: 温度值(范围:0~50°)
+ * @param {uint8_t} *humi: 湿度值(范围:20%~90%)
+ * @return: {*} 0[正常]/1[读取失败]
+ */
 uint8_t DHT11_Read_Data(uint8_t *temp, uint8_t *humi)
 {
     uint8_t buf[5];
     uint8_t i;
-    DHT11_Rst();
+    DHT11_Reset();
     if (DHT11_Check() == 0)
     {
         for (i = 0; i < 5; i++) // 读取40位数据
@@ -96,15 +117,19 @@ uint8_t DHT11_Read_Data(uint8_t *temp, uint8_t *humi)
             *humi = buf[0];
             *temp = buf[2];
         }
+        return 0;
     }
     else
+    {
         return 1;
-    return 0;
+    }
 }
 
-// 初始化DHT11的IO口 DQ 同时检测DHT11的存在
-// 返回1:不存在
-// 返回0:存在
+/**
+ * @description: DHT11初始化
+ *      初始化DHT11的IO口 & 检测DHT11的存在
+ * @return: {*} 0[存在]/1[不存在]
+ */
 uint8_t DHT11_Init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
@@ -115,6 +140,6 @@ uint8_t DHT11_Init(void)
     GPIO_Init(GPIOA, &GPIO_InitStructure); // 初始化IO口
     GPIO_SetBits(GPIOA, DHT_PIN);          // PG11 输出高
 
-    DHT11_Rst();          // 复位DHT11
+    DHT11_Reset();          // 复位DHT11
     return DHT11_Check(); // 等待DHT11的回应
 }
--- User/main.c
+++ User/main.c
@@ -3,13 +3,18 @@
 #include "OLED.h"
 #include "LED.h"
 #include "Key.h"
+#include "DHT11.h"
 
 int main(void)
 {
 	OLED_Init();		  // OLED初始化
 	Key_Init(GPIO_Pin_0); // Key初始化
+	DHT11_Init();		  // DHT11初始化
 
 	while (1)
 	{
+		OLED_ShowString(1, 0, "DHT11 init OK   ");
+		Delay_ms(100);
 	}
+	OLED_ShowString(3, 0, "Hardware init OK    ");
 }
